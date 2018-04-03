02f51c5501104dfe73451bb2ce4c63da02f7346f
amelda <amelda@sabanciuniv.edu>
2018-04-04 02:05:40 +0300

    amelda | Created empty application

---
+++ SUMall/app/src/main/java/edu/sabanciuniv/cs310/sumall/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.cs310.sumall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
